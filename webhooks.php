d70980d6137f9abeb697da5f5fc7fe0486227fee
maxtri <maxtriname@gmail.com>
2018-10-12 10:54:56 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -21,54 +21,45 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
-			$table = [
-			   case "t_b":
-    			   // กำหนด action 4 ปุ่ม 4 ประเภท
-			    $actionBuilder = array(
-				new MessageTemplateActionBuilder(
-				    'Message Template',// ข้อความแสดงในปุ่ม
-				    'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-				),
-				new UriTemplateActionBuilder(
-				    'Uri Template', // ข้อความแสดงในปุ่ม
-				    'https://www.ninenik.com'
-				),
-				new DatetimePickerTemplateActionBuilder(
-				    'Datetime Picker', // ข้อความแสดงในปุ่ม
-				    http_build_query(array(
-					'action'=>'reservation',
-					'person'=>5
-				    )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-				    'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
-				    substr_replace(date("Y-m-d H:i"),'T',10,1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
-				    substr_replace(date("Y-m-d H:i",strtotime("+5 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
-				    substr_replace(date("Y-m-d H:i"),'T',10,1) //วันที่ เวลา น้อยสุดที่เลือกได้
-				),      
-				new PostbackTemplateActionBuilder(
-				    'Postback', // ข้อความแสดงในปุ่ม
-				    http_build_query(array(
-					'action'=>'buy',
-					'item'=>100
-				    )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-				    'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-				),      
-			    );
-			    $imageUrl = 'https://www.mywebsite.com/imgsrc/photos/w/simpleflower';
-			    $replyData = new TemplateMessageBuilder('Button Template',
-				new ButtonTemplateBuilder(
-					'button template builder', // กำหนดหัวเรื่อง
-					'Please select', // กำหนดรายละเอียด
-					$imageUrl, // กำหนด url รุปภาพ
-					$actionBuilder  // กำหนด action object
-				)
-			    );              
-			    break; 
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}else if ($event['type'] == 'btnname' && $event['btnname']['type'] == 'button') {
+			// Get text sent
+			$text = $event['source']['userId'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Build message to reply back
+			$actions = [];
+			foreach ($this->actionBuilders as $actionBuilder) {
+			    $actions[] = $actionBuilder->buildTemplateAction();
+			}
+			$messages = [
+				'type' => TemplateType::BUTTONS,
+				 'thumbnailImageUrl' => $this->thumbnailImageUrl,
+				 'title' => $this->title,
+				 'text' => $this->text,
+				 'actions' => $actions,
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$table],
+				'btnname' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
