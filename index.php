2cd49574d8aed62dd017b6e72e286c8e6c0a6514
maxtri <maxtriname@gmail.com>
2018-10-20 13:48:03 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if ( sizeof($request_array['events']) > 0 )
       $reply_message = 'แสดง B';
       break;
       case 'tm':
-      $$reply_message = new TemplateMessageBuilder('Confirm Template',
+      $$replyData = new TemplateMessageBuilder('Confirm Template',
         new ConfirmTemplateBuilder(
           'Confirm template builder',
           array(
@@ -82,7 +82,7 @@ if(strlen($reply_message) > 0)
    //$reply_message = iconv("tis-620","utf-8",$reply_message);
  $data = [
   'replyToken' => $reply_token,
-  'messages' => [['type' => 'text', 'text' => $reply_message]]
+  'messages' => [['type' => 'text', 'text' => $reply_message,$replyData]]
 ];
 $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
