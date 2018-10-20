cb85c780a8e07b3d79f0b4af85cdc301c59e24c1
maxtri <maxtriname@gmail.com>
2018-10-20 11:09:10 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -22,8 +22,10 @@ if ( sizeof($request_array['events']) > 0 )
    if( $event['message']['type'] == 'text' )
    {
     $testdb = 'select * from users where id = 1';
+    foreach ($testdb as $a) {
+    }
     $text = $event['message']['text'];
-    $reply_message = 'ระบบได้รับข้อความ ('.$testdb.') ของคุณแล้ว';
+    $reply_message = 'ระบบได้รับข้อความ ('.$a->id.') ของคุณแล้ว';
    }
    else
     $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
