b05d018b62e3c37336ffa71c23156b4514702877
maxtri <maxtriname@gmail.com>
2018-10-23 14:52:47 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -26,12 +26,6 @@ if (sizeof($request_array['events']) > 0 )
    {
     $textname = $event['source']['userId']; // เอา user id ของแต่ละคนที่ทักมาหาบอทตัวนี้เก็บไว้
     $text = $event['message']['text'];
-    $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
-    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-    $arrayPostData['messages'][0]['type'] = "image";
-    $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
-    $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
-    replyMsg($arrayHeader,$arrayPostData);
     switch ($text) {
       case 'A':
       $reply_message = 'แสดง A '.$textname.'';
@@ -39,14 +33,23 @@ if (sizeof($request_array['events']) > 0 )
       case 'B':
       $reply_message = 'แสดง B '.$textname.'';
       break;
-      case 'C':
-      $reply_message = ''.$image_url.'';
-      break;
       default:
       $reply_message = ''.$textname.'';
       break; 
     }
   }
+}
+#ตัวอย่าง Message Type "Image"
+else if($text){
+  if ($event['message']['type'] == 'image'){
+    $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "image";
+    $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
+    $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
+    $test2 = replyMsg($arrayHeader,$arrayPostData);
+    $reply_message = 'รูป '.$test2.'';
+  } 
 }
 
 if(strlen($reply_message) > 0)
