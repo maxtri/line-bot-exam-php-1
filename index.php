8b146acdb4bbc4cf580d8247af0e201c26594bca
maxtri <maxtriname@gmail.com>
2018-10-23 15:25:51 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$ACCESS_TOKEN}"
+
 if (sizeof($request_array['events']) > 0 )
 {
  foreach ($request_array['events'] as $event)
@@ -19,7 +23,6 @@ if (sizeof($request_array['events']) > 0 )
    {
     $textname = $event['source']['userId']; // เอา user id ของแต่ละคนที่ทักมาหาบอทตัวนี้เก็บไว้
     $text = $event['message']['text'];
-    $image = 'https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg';
     switch ($text) {
       case 'A':
       $reply_message = 'แสดง A '.$textname.'';
@@ -28,7 +31,12 @@ if (sizeof($request_array['events']) > 0 )
       $reply_message = 'แสดง B '.$textname.'';
       break;
       case 'C':
-      $reply_message = 'แสดง C '.$image.'';
+      $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+      $arrayPostData['replyToken'] = $request_array['events'][0]['replyToken'];
+      $arrayPostData['messages'][0]['type'] = "image";
+      $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
+      $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
+      replyMsg($arrayHeader,$arrayPostData);
       break;
       default:
       $reply_message = ''.$textname.'';
@@ -67,5 +75,20 @@ function send_reply_message($url, $post_header, $post_body)
 
  return $result;
 }
+
+function replyMsg($arrayHeader,$arrayPostData){
+  $strUrl = "https://api.line.me/v2/bot/message/reply";
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL,$strUrl);
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+  curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  $result = curl_exec($ch);
+  curl_close ($ch);
+}
+exit;
 
 ?>
